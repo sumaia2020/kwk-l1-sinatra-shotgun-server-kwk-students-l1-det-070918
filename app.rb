ad8078500c54a9b5dcf77535bcecf2032dda8e3a
sumaia2020 <sbalghaiti@hawkmail.hfcc.edu>
2018-07-17 18:29:03 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,11 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
+<<<<<<< HEAD
     "Started my server using Shotgun! "
+=======
+    "Sumaia! "
+>>>>>>> 817a778e8788dbba709b63ca2b92db15d5dd84fc
   end
 
 end
